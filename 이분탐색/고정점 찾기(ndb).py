9ea224a69df55ea26c533b32fe19a0869b0684b0
rngustj9139 <mnb9139@khu.ac.kr>
2022-11-29 09:44:49 +0900

    고정점 찾기(ndb) 재귀함수 버전

--- 이분탐색/고정점 찾기(ndb).py
+++ 이분탐색/고정점 찾기(ndb).py
@@ -1,25 +1,51 @@
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# numberList = list(map(int, input().split()))
+#
+# numberList.sort()
+# startIndex = 0
+# endIndex = N - 1
+# findCheck = True
+#
+# while (startIndex <= endIndex):
+#     mid = (startIndex + endIndex) // 2
+#
+#     if (numberList[mid] == mid):
+#         print(mid)
+#         findCheck = False
+#         break
+#     elif (numberList[mid] < mid):
+#         startIndex = mid + 1
+#     elif (numberList[mid] > mid):
+#         endIndex = mid - 1
+#
+# if (findCheck == True):
+#     print(-1)
+
 import sys
 input = sys.stdin.readline
+
+def binary_search(array, start, end):
+    if (start > end):
+        return None
+
+    mid = (start + end) // 2
+
+    if (array[mid] == mid):
+        return mid
+    elif (array[mid] < mid):
+        return binary_search(array, start, mid - 1)
+    elif (array[mid] > mid):
+        return binary_search(array, mid + 1, end)
 
 N = int(input())
 numberList = list(map(int, input().split()))
 
-numberList.sort()
-startIndex = 0
-endIndex = N - 1
-findCheck = True
+result = binary_search(numberList, 0, N - 1)
 
-while (startIndex <= endIndex):
-    mid = (startIndex + endIndex) // 2
-
-    if (numberList[mid] == mid):
-        print(mid)
-        findCheck = False
-        break
-    elif (numberList[mid] < mid):
-        startIndex = mid + 1
-    elif (numberList[mid] > mid):
-        endIndex = mid - 1
-
-if (findCheck == True):
+if (result == None):
     print(-1)
+else:
+    print(result)
