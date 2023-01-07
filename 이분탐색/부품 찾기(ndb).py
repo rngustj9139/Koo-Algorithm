ed8e868f792e712aa20ef9b650e3c5879bb72cdf
rngustj9139 <mnb9139@khu.ac.kr>
2023-01-07 23:34:30 +0900

    부품찾기(ndb)

--- 이분탐색/부품 찾기(ndb).py
+++ 이분탐색/부품 찾기(ndb).py
@@ -2,4 +2,29 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-storeNumberList = list(map(int, input().split()))
+equipmentNumberList = list(map(int, input().split()))
+M = int(input())
+requireNumberList = list(map(int, input().split()))
+result = []
+
+for i in range(M):
+    s = 0
+    e = N - 1
+
+    while (True):
+        m = (s + e) // 2
+
+        if (equipmentNumberList[m] > requireNumberList[i]):
+            e = m - 1
+        elif (equipmentNumberList[m] < requireNumberList[i]):
+            s = m + 1
+        else:
+            result.append("yes")
+            break
+
+        if (s > e):
+            result.append("no")
+            break
+
+for i in range(M):
+    print(result[i], end = ' ')
