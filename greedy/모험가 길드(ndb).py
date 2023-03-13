825aaef83ac12828a19c633c648554fcbd10ca3a
rngustj9139 <mnb9139@khu.ac.kr>
2023-03-14 00:10:17 +0900

    모험가 길드(ndb)

---
+++ greedy/모험가 길드(ndb).py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+arr = list(map(int, input().split()))
+cnt = 0
+result = 0
+
+arr.sort() # 오름차순 정렬
+
+for i in arr:
+    cnt += 1
+    if (cnt >= i):
+        result += 1
+        cnt = 0
+
+print(result)
+
