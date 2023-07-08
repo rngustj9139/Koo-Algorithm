09d03c4c8d8043ea262d39790c911796943e885c
rngustj9139 <mnb9139@khu.ac.kr>
2023-07-08 10:35:51 +0900

    문자열 압축(ndb)

---
+++ 구현/문자열 압축(ndb).py
@@ -0,0 +1,37 @@
+'''
+[입출력 예시]
+aabbaccc, 2a2ba3c (1개 단위), 7개
+ababcdcdababcdcd, 2ababcdcd (8개 단위), 9개
+abcabcdede, 2abcdede (3개 단위), 8개
+'''
+
+def solution(s):
+    answer = len(s)
+
+    for step in range(1, (len(s) // 2) + 1):
+        prev = s[0:step]
+        cnt = 1
+        compressed = ""
+        for i in range(step, len(s), step):
+            if (prev == s[i:i + step]): # 만약 이전의 값과 똑같은 경우
+                cnt += 1
+            else: # 만약 이전의 값과 다른 경우
+                if (cnt >= 2):
+                    compressed += str(cnt) + prev
+                else:
+                    compressed += prev
+                prev = s[i:i + step]
+                cnt = 1
+        # 남은 값 처리
+        if (cnt >= 2):
+            compressed += str(cnt) + prev
+        else:
+            compressed += prev
+
+        answer = min(answer, len(compressed))
+
+    return answer
+
+s = input()
+
+print(solution(s))
